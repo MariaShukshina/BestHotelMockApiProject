19433433edca766bf63ea8743ccb9e1d3d62b27a
Maria Shukshina <shukshina.mariia@gmail.com>
2023-09-12 18:08:03 +0200

    Set up and designed the last screen

--- app/src/main/java/com/example/besthoteldemoproject/ui/SuccessFragment.kt
+++ app/src/main/java/com/example/besthoteldemoproject/ui/SuccessFragment.kt
@@ -5,29 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.besthoteldemoproject.R
+import com.example.besthoteldemoproject.databinding.FragmentSuccessBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SuccessFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SuccessFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentSuccessBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_success, container, false)
+    ): View {
+        binding = FragmentSuccessBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.successToolbar.setupWithNavController(navController, appBarConfiguration)
+
+        val randomInt = (100000..200000).random()
+
+        binding.confirmationTextView.text = getString(R.string.confirmation_text, randomInt)
+
+        binding.successButton.setOnClickListener {
+            navController.popBackStack(R.id.hotelFragment, false)
+        }
     }
 
 }
